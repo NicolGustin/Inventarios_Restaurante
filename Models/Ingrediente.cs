85d9599fb0edd5c07073e3cf2976018fd63e881e
Nicol Gustin <nicol930519@hotmail.com>
2024-05-21 23:55:36 -0500

    ingrediente

---
+++ Models/Ingrediente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inventarios_Restaurante.Models
+{
+    public class Ingrediente
+    {
+        //[Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        [Column(TypeName ="decimal(6,2)")]
+        public double CantidadDisponible { get; set; }
+        public string UnidadMedida { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
