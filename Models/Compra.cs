7151b8be8a099adbb90765b61dd913b7e097d861
Nicol Gustin <nicol930519@hotmail.com>
2024-05-22 22:53:53 -0500

    Compra Models

--- Models/Compra.cs
+++ Models/Compra.cs
@@ -2,5 +2,14 @@
 {
     public class Compra
     {
+        public int Id { get; set; }
+        public int IngredienteId { get; set; }
+        public int ProveedorId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public DateTime FechaCompra { get; set; }
+
+        public Ingrediente Ingrediente { get; set; }
+        public Proveedor proveedor { get; set; }
     }
 }
--- obj/Debug/net8.0/Inventarios_Restaurante.AssemblyInfo.cs
+++ obj/Debug/net8.0/Inventarios_Restaurante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventarios_Restaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbf084e735454869bc77d16dd4947239a25a4e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed0b39b66025ec528349b34a8061883e90f2fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventarios_Restaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventarios_Restaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
